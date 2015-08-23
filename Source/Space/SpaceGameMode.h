95c2c09ae09a16ff3ae4f4c7fc80392372c7a740
Tamuel <eastern7star@gmail.com>
2015-08-24 00:56:36 +0900

    Initialize Project Space
    Upload whole Space files.

---
+++ Source/Space/SpaceGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SpaceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACE_API ASpaceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
